15c15c7e17d8f4ec1a063f2cf1519ec15dc067ad
dhanushkrock@gmail.com <dhanushkrock@gmail.com>
2026-02-04 12:14:30 +0530

    Fix build: Add firebase to prod env and increase budgets

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://YOUR_BACKEND_APP.onrender.com' // You will update this after deploying the backend
+    apiUrl: 'https://tt-backend-6kjc.onrender.com',
+    firebase: {
+        apiKey: "AIzaSyAhdHImJQy_0b6xSv4YAVyt1oxUtfp3Sy8",
+        authDomain: "tamilnadu-tourism-dc0bf.firebaseapp.com",
+        projectId: "tamilnadu-tourism-dc0bf",
+        storageBucket: "tamilnadu-tourism-dc0bf.firebasestorage.app",
+        messagingSenderId: "374458333312",
+        appId: "1:374458333312:web:ba95255d98b8830a4e4e65",
+        measurementId: "G-7VCBN1NHZH"
+    }
 };
